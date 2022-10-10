32b2c4c24a3a12a955eb9e0a1ef36a1afb158fb4
Artem Prygunov <harry177@rambler.ru>
2022-10-11 01:47:49 +0200

    fix: correct burger menu for donate-page; feat: add slider for pets block

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,3 +1,6 @@
+/* Burger-menu */
+
+
 const burgerIcon = document.querySelector(".burger__menu");
 const burgerMenu = document.querySelector(".burger__menu__active");
 const darkTheme = document.querySelector(".dark");
@@ -19,3 +22,84 @@ burgerCross.addEventListener('click', function() {
     burgerMenu.classList.remove("show");
     darkTheme.classList.remove("dark__active");
 })
+
+
+/* Slider animals */
+
+const rbLeft = document.querySelector(".rb__left");
+const rbRight = document.querySelector(".rb__right");
+const carousel = document.querySelector(".animals__container");
+const leftPart = document.querySelector(".animals__left");
+const centerPart = document.querySelector(".animals__center");
+const rightPart = document.querySelector(".animals__right");
+const blockPanda = document.getElementById("ab__panda");
+const blockEagle = document.getElementById("ab__eagle");
+const blockGorilla = document.getElementById("ab__gorilla");
+const blockSloth = document.getElementById("ab__sloth");
+const blockCheetah = document.getElementById("ab__cheetah");
+const blockPenguin = document.getElementById("ab__penguin");
+const blockAlligator = document.getElementById("ab__alligator");
+
+
+let blocks = [blockPanda, blockEagle, blockGorilla, blockSloth, blockCheetah, blockPenguin]
+
+
+function mix(arr) {
+    arr.sort(() => Math.random() - 0.5);
+  }
+
+
+function setSlider() {
+  mix(blocks);
+
+  leftPart.innerHTML = "";
+  rightPart.innerHTML = "";
+
+  for (let i = 0; i < 6; i++) {
+    const leftBlock = document.createElement('div');
+    leftBlock.classList.add("animal__wrapper");
+    leftBlock.innerHTML = blocks[i].innerHTML;
+    leftPart.append(leftBlock);
+
+    const rightBlock = document.createElement('div');
+    rightBlock.classList.add("animal__wrapper");
+    rightBlock.innerHTML = blocks[i].innerHTML;
+    rightPart.append(rightBlock);
+  }
+};
+
+window.addEventListener('load', setSlider());
+
+const moveLeft = () => {
+    carousel.classList.add("transition-left");
+    rbLeft.removeEventListener("click", moveLeft);
+    rbRight.removeEventListener("click", moveRight);
+  };
+
+  rbLeft.addEventListener("click", moveLeft);
+
+const moveRight = () => {
+    carousel.classList.add("transition-right");
+    rbLeft.removeEventListener("click", moveLeft);
+    rbRight.removeEventListener("click", moveRight);
+  };
+
+  rbRight.addEventListener("click", moveRight);
+
+
+  carousel.addEventListener("animationend", function(animationEvent) {
+    const leftContent = document.querySelector(".animals__left").innerHTML;
+    const rightContent = document.querySelector(".animals__right").innerHTML;
+  
+    if (animationEvent.animationName === "move-left") {
+      carousel.classList.remove("transition-left");
+      centerPart.innerHTML = leftContent;
+    } else {
+      carousel.classList.remove("transition-right");
+      centerPart.innerHTML = rightContent;
+    }
+  
+    rbLeft.addEventListener("click", moveLeft);
+    rbRight.addEventListener("click", moveRight);
+    setSlider();
+  })
